6c7e6a7811f7e0e5c78d89201879729c93831fa6
Arthur Dias <arthurdias0209@gmail.com>
2024-01-04 16:56:36 -0300

    refazendo o projeto

--- Trocar de cores/script.js
+++ Trocar de cores/script.js
@@ -1,16 +1,19 @@
-const colors = ['blue', 'black', 'green', 'red', 'purple', 'yellow', 'pink', 'brown', 'gray', '#ddd']
 const buttonColor = document.querySelector('button')
 const colortext = document.querySelector('.span-color')
 
 buttonColor.addEventListener('click', function() {
-    // get random number between 0 - 3
-    const randomNumber = getRandomNumber();
-    console.log(getRandomNumber())
+    const color = random_color();
 
-    document.body.style.backgroundColor = colors[randomNumber]
-    colortext.textContent = colors[randomNumber]
+    document.body.style.backgroundColor = color
+    colortext.textContent = color
 })
 
-function getRandomNumber() {
-    return Math.floor(Math.random() * colors.length)
+function random_color() {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+
+    const rgb = `rgb(${r}, ${g}, ${b})`
+
+    return rgb
 }
